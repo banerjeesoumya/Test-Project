21a89d063f60e6a86c762b43af1a7505b4fe3431
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2025-09-25 00:02:29 +0530

    Added the authentication routes and the pages

---
+++ my-app/src/modules/auth/ui/views/sign-in-view.tsx
@@ -0,0 +1,144 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+import { Card, CardContent } from "@/components/ui/card";
+import { OctagonAlertIcon } from "lucide-react";
+import { authClient } from "@/lib/auth-client";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+
+
+const formSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(1, "Password is required"),
+})
+
+export const SignInView = () => {
+    const router = useRouter();
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        }
+    })
+
+    const onSubmit = (data: z.infer<typeof formSchema>) => {
+        setError(null);
+        setLoading(true);
+        authClient.signIn.email(
+            {
+                email: data.email,
+                password: data.password,
+            },
+            {
+                onSuccess: () => {
+                    setLoading(false);
+                    router.push("/");
+                },
+                onError: ({ error }) => {
+                    setError(error.message);
+                    setLoading(false);
+                }
+            },
+        )
+    }
+
+    return (
+        <div className="flex flex-col gap-6">
+            <Card className="overflow-hidden p-0">
+                <CardContent className="grid p-0 md:grid-cols-2">
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="p-6 md:p-8">
+                            <div className="flex flex-col gap-6">
+                                <div className="flex flex-col items-center text-center">
+                                    <h1 className="text-2xl font-bold">
+                                        Welcome Back
+                                    </h1>
+                                    <p className="text-muted-foreground text-balance">
+                                        Login to your account
+                                    </p>
+                                </div>
+                                <div className="grid gap-3">
+                                    <FormField 
+                                        control={form.control}
+                                        name="email"
+                                        render={({ field }) => (    
+                                            <FormItem>
+                                                <FormLabel>Email</FormLabel>
+                                                <FormControl>
+                                                    <Input type="email" placeholder="you@example.com" {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+                                <div className="grid gap-3">
+                                    <FormField 
+                                        control={form.control}
+                                        name="password"
+                                        render={({ field }) => (    
+                                            <FormItem>
+                                                <FormLabel>Password</FormLabel>
+                                                <FormControl>
+                                                    <Input type="password" placeholder="••••••••" {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+                                {!!error && (
+                                    <Alert className="bg-destructive/10 border-none">
+                                        <OctagonAlertIcon className="h-4 w-4 !text-destructive" />
+                                        <AlertTitle>{error}</AlertTitle>
+                                    </Alert>
+                                )}
+                                <Button disabled={loading} type="submit" className="w-full">
+                                    Sign In
+                                </Button>
+                                <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                                    <span className="bg-card text-muted-foreground relative z-10 px-2">
+                                        Or continue with
+                                    </span>
+                                </div>
+                                <div className="grid grid-cols-2 gap-4">
+                                    <Button variant="outline" className="w-full" type="button">
+                                        Google
+                                    </Button>
+                                    <Button variant="outline" className="w-full" type="button">
+                                        GitHub
+                                    </Button>
+                                </div>
+                                <div className="text-center text-sm text-muted-foreground">
+                                    Don&#39;t have an account? {" "}
+                                    <Link href="/sign-up" className="underline underline-offset-4 hover:text-primary">
+                                        Sign Up
+                                    </Link>
+                                </div>
+                            </div>
+                        </form>
+                    </Form>
+                    <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+                        <img src="/logo.svg" alt="Image"  className="h-[92px] w-[92px]"/>
+                        <p className="text-2xl font-semibold text-white"> 
+                            Product Name
+                        </p>
+                    </div>
+                </CardContent>
+            </Card>
+        </div>
+    );
+};
